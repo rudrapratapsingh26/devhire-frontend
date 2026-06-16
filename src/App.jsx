1747bf5803f724cd6fb672206c4b369c03a0265b
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-06-16 20:58:41 +0530

    feat: add job detail route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Signup from "./pages/auth/Signup";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import CheckEmail from "./pages/auth/CheckEmail";
 import ResetPassword from "./pages/auth/ResetPassword";
-import JobFeed from "./pages/candidate/JobFeed";
+import JobFeed from "./components/candidate/JobFeed";
+import JobDetail from "./pages/candidate/JobDetail";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/check-email" element={<CheckEmail />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/candidate/jobs" element={<JobFeed />} />
+          <Route path="/candidate/jobs/:id" element={<JobDetail />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
